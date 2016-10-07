d8c1d86c057d38d67397010834593c3999751cb0
Henric Bernhardsson <henrictrc@hotmail.com>
2016-10-07 12:21:23 +0200

    ny

--- string formatering/Program.cs
+++ string formatering/Program.cs
@@ -10,6 +10,23 @@ namespace string_formatering
     {
         static void Main(string[] args)
         {
+            //övning1
+            DateTime dt = DateTime.Today;
+            Console.WriteLine(dt.ToString("D"));
+            //övning2
+            double cirkel = Math.PI * 4 * 4;
+            Console.WriteLine(cirkel.ToString("F4"));
+            //övning3
+            int tal = 1000000000;
+            Console.WriteLine(tal.ToString("0,0,0"));
+            //övning4
+            double value = 123.00;
+            Console.WriteLine(value.ToString("\\#\\#\\# ### kr och \\0\\0 öre \\#\\#\\# "));
+            //övning5
+            Console.WriteLine("skriv in personnummer");
+            string pnmr = Console.ReadLine();
+            decimal persontal = decimal.Parse(pnmr);
+            Console.WriteLine(persontal.ToString(""));
         }
     }
 }
